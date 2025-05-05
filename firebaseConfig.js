417edf8b4f6df9ff941dfeb35028a2966c6ed18d
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2025-05-04 21:33:20 -0300

    ss

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCRusbXWarMggnNjvhYfULL4STH-wlB0p8",
+    authDomain: "lista-de-compras-217e4.firebaseapp.com",
+    projectId: "lista-de-compras-217e4",
+    storageBucket: "lista-de-compras-217e4.firebasestorage.app",
+    messagingSenderId: "701419963495",
+    appId: "1:701419963495:web:70cf077322feea44bec0c2"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
